d6ebec19215162091fd8807ff5b0ce98d59d494c
strong <marlon.guao@gmail.com>
2024-11-02 17:31:49 +0100

    updated init.lua

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -24,17 +24,6 @@ vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
 
-  -- render-markdown
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    ---@module 'render-markdown'
-    ---@type render.md.UserConfig
-    opts = {},
-},
-
 
   -- Catppuccino 
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
@@ -107,11 +96,6 @@ local plugins = {
 local opts = {}
 
 require("lazy").setup(plugins, opts)
-
-require('render-markdown').setup({
-    render_modes = true,
-    enabled = true,
-})
 
 require('lualine').setup {
   options = {
